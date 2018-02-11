a8645c7bb250e2bd8eab374a1fb900a86634739e
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-02-11 17:32:34 +0200

    update files

--- stockMarket/homePage/models.py
+++ stockMarket/homePage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class users (models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=16)
+    email = models.CharField(max_length=100)
+    balance = models.FloatField()
