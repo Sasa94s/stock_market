048ca212a6585ea4073be8131ccdc25dabc28146
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-03-17 19:57:24 +0200

    Adding mail service, creating templates for (password_change, reset_password, email verification)

--- stockMarket/home/views.py
+++ stockMarket/home/views.py
@@ -4,6 +4,3 @@ from allauth.account.views import SignupView, LoginView, PasswordResetView
 # Create your views here.
 class home(generic.TemplateView):
     template_name = 'home/home.html'
-
-class MyLoginView(LoginView):
-    template_name = 'allauth/accounts/login.html'
