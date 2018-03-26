c23e12afc731065b431f743669f22042a4d04b72
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-03-26 03:00:50 +0200

    Cleanning templates

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -2,14 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
-
 # Create your models here.
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='user', on_delete=models.PROTECT)
-    # photo = FileField(verbose_name=_("Profile Picture"),
-    #                   upload_to=upload_to("main.UserProfile.photo", "profiles"),
-    #                   format="Image", max_length=255, null=True, blank=True)
     website = models.URLField(default='', blank=True)
     bio = models.TextField(default='', blank=True)
     phone = models.CharField(max_length=20, blank=True, default='')
@@ -20,19 +15,10 @@ class UserProfile(models.Model):
     def __str__(self):
       return str(self.user)
 
-
 def create_profile(sender, **kwargs):
     user = kwargs["instance"]
     if kwargs["created"]:
         user_profile = UserProfile(user=user)
         user_profile.save()
 
-
 post_save.connect(create_profile, sender=User)
-
-
-     # def __str__(self):
-     #     return self.name
-
-
-
